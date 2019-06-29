bde6979f6b7f52b25b0c1e57e0cc7da3d23ec12f
Ladawn Rorschach <lrorscha@at-m7.21-school.ru>
2019-06-29 20:45:26 +0300

    Final

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lrorscha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 22:06:59 by lrorscha          #+#    #+#             */
-/*   Updated: 2019/06/29 20:40:01 by lrorscha         ###   ########.fr       */
+/*   Updated: 2019/06/29 20:45:20 by lrorscha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int		ft_new_line(char **s, char **line, int fd, int ret)
 
 int		get_next_line(const int fd, char **line)
 {
-	static char	*s[65535];
+	static char	*s[1024];
 	char		buf[BUFF_SIZE + 1];
 	char		*temp;
 	int			ret;
